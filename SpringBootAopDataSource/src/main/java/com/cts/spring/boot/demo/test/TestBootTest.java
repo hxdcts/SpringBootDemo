d0923cafa3177c5fce45af7b2e62c36e581004d3
Cts <caotieshuan1986@163.com>
2018-01-15 16:21:29 +0800

    test

--- SpringBootAopDataSource/src/main/java/com/cts/spring/boot/demo/test/TestBootTest.java
+++ SpringBootAopDataSource/src/main/java/com/cts/spring/boot/demo/test/TestBootTest.java
@@ -1,6 +1,10 @@
 package com.cts.spring.boot.demo.test;
 
+import org.apache.commons.logging.Log;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class TestBootTest {
 
 	public static void main(String[] args) {
